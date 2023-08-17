47ccd75993213350fef5c112c122df436b8b8d99
KareemMarzouk <bkmarzouk@outlook.com>
2023-08-17 14:15:58 +0100

    proto validation check for input paths

--- src/SOPRANO/run_local_SSB_selection.py
+++ src/SOPRANO/run_local_SSB_selection.py
@@ -6,6 +6,10 @@ def main(*args, **kwargs):
     pass
 
 
+def _validate_input_path(cli_path: pathlib.PosixPath, optional=False):
+    pass
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="SOPRANO input arguments")
 
@@ -15,7 +19,7 @@ if __name__ == "__main__":
         dest="input",
         type=pathlib.PosixPath,
         help="Prove the path to the input VEP annotated file.",
-        required=True
+        required=True,
     )
 
     parser.add_argument(
@@ -24,8 +28,8 @@ if __name__ == "__main__":
         dest="bed_file",
         type=pathlib.PosixPath,
         help="Provide the path to the bed file with protein coordinates named "
-             "by Transcript (ENSTXXXXXX 123 135)",
-        required=True
+        "by Transcript (ENSTXXXXXX 123 135)",
+        required=True,
     )
 
     parser.add_argument(
@@ -34,8 +38,8 @@ if __name__ == "__main__":
         dest="output",
         type=pathlib.PosixPath,
         help="Provide the path to the output directory in which dN/dS results "
-             "will be cached.",
-        required=True
+        "will be cached.",
+        required=True,
     )
 
     parser.add_argument(
@@ -44,7 +48,7 @@ if __name__ == "__main__":
         dest="name",
         type=str,
         help="Provide an identifying name for your results.",
-        required=True
+        required=True,
     )
     analysis_params_group = parser.add_argument_group()
 
@@ -52,14 +56,14 @@ if __name__ == "__main__":
         "-t",
         dest="bed_regions",
         type=pathlib.PosixPath,
-        help="Provide a bed file with regions to randomize."
+        help="Provide a bed file with regions to randomize.",
     )
 
     analysis_params_group.add_argument(
         "--use_ssb192",
         dest="use_ssb192",
         action="store_true",
-        help="If flag is used ssb192 will be used, otherwise ssb7."
+        help="If flag is used ssb192 will be used, otherwise ssb7.",
     )
 
     analysis_params_group.add_argument(
@@ -67,15 +71,19 @@ if __name__ == "__main__":
         dest="use_random",
         action="store_true",
         help="If flag is used, calculate a dNdS calue for a random region "
-             "similar to the target."
+        "similar to the target.",
     )
 
     analysis_params_group.add_argument(
         "--exclude_drivers",
         action="store_true",
         help="If flag is used, driver geners will be excluded from the "
-             "calculation."
+        "calculation.",
     )
 
     args = parser.parse_args()
 
+    _validate_input_path(args.input)
+    _validate_input_path(args.bed_file)
+    _validate_input_path(args.output)
+    _validate_input_path(args.bed_regions, optional=True)
