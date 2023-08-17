35f1c5f960e2b1047719d706662526ff82e2d257
KareemMarzouk <bkmarzouk@outlook.com>
2023-08-17 14:03:27 +0100

    optional args parsing

--- src/SOPRANO/run_local_SSB_selection.py
+++ src/SOPRANO/run_local_SSB_selection.py
@@ -45,5 +45,34 @@ if __name__ == "__main__":
         help="Provide an identifying name for your results.",
         required=True
     )
+    analysis_params_group = parser.add_argument_group()
+
+    analysis_params_group.add_argument(
+        "-t",
+        dest="bed_regions",
+        help="Provide a bed file with regions to randomize."
+    )
+
+    analysis_params_group.add_argument(
+        "--use_ssb192",
+        dest="use_ssb192",
+        action="store_true",
+        help="If flag is used ssb192 will be used, otherwise ssb7."
+    )
+
+    analysis_params_group.add_argument(
+        "--use_random",
+        dest="use_random",
+        action="store_true",
+        help="If flag is used, calculate a dNdS calue for a random region "
+             "similar to the target."
+    )
+
+    analysis_params_group.add_argument(
+        "--exclude_drivers",
+        action="store_true",
+        help="If flag is used, driver geners will be excluded from the "
+             "calculation."
+    )
 
     args = parser.parse_args()
