bcaeca1cd0ca44e87886b9220be76582e7fecbe9
KareemMarzouk <bkmarzouk@outlook.com>
2023-08-21 12:30:46 +0100

    skeleton functions for STEP 2

---
+++ src/SOPRANO/obtain_fasta_regions.py
@@ -0,0 +1,28 @@
+def get_target_regions(*args, **kwargs):
+    """
+    implement line 159
+    :param args:
+    :param kwargs:
+    :return:
+    """
+    pass
+
+
+def get_non_target_regions(*args, **kwargs):
+    """
+    Implement line line 160
+    :param args:
+    :param kwargs:
+    :return:
+    """
+    pass
+
+
+def get_transcript_regions_for_site_numbers(*args, **kwargs):
+    """
+    Implment lines 163-164
+    :param args:
+    :param kwargs:
+    :return:
+    """
+    pass
