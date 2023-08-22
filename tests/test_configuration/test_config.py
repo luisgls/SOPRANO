ae9f757b2328ea1ebb6a3481b1ca144dcd5ecc4d
KareemMarzouk <bkmarzouk@outlook.com>
2023-08-22 10:56:51 +0100

    update tests for setup_conda.py

--- tests/test_configuration/test_config.py
+++
@@ -1,6 +0,0 @@
-def test_running_conda():
-    """
-    TODO: Check whether python interpreter is linked to conda env
-    :return:
-    """
-    pass
---
+++ tests/test_configuration/test_setup_conda.py
@@ -0,0 +1,18 @@
+def test_running_conda():
+    """
+    TODO: Check whether python interpreter is linked to conda env
+    :return:
+    """
+    pass
+
+
+def test__create_new_condarc():
+    assert False
+
+
+def test__update_condarc():
+    assert False
+
+
+def test_prepare_condarc():
+    assert False
