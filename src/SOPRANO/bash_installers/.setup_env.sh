c03f2caa1bf8d810b9b5456b4df700d249bff92c
KareemMarzouk <bkmarzouk@outlook.com>
2023-08-23 10:47:57 +0100

    bash installers

---
+++ src/SOPRANO/bash_installers/.setup_env.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# Mamba/conda path
+_mamba_path=$(which mamba)
+_conda_path=$(which conda)
+_env_grep=$(conda env list | grep "soprano-dev")
+
+yml_path="src/SOPRANO/local.yml"
+
+function deactivate_all() {
+  for _ in $(seq "${CONDA_SHLVL}")
+  do
+    conda deactivate
+  done
+}
+
+if [ -n "$_mamba_path" ]
+then
+	echo "-- mamba detected"
+  deactivate_all
+
+	if [ -z "$_env_grep" ]
+	then
+		echo "-- building environment"
+		mamba env create -f $yml_path
+    echo "-- activating soprano"
+    mamba activate soprano-dev
+	else
+		echo "-- soprano environment detected"
+    echo "-- activating soprano"
+    mamba activate soprano-dev
+		echo "-- checking for updates to environment"
+		mamba env update --file $yml_path --prune
+	fi
+
+elif [ -n "$_conda_path" ]
+then
+	echo "-- conda detected"
+  deactivate_all
+	if [ -z "$_env_grep" ]
+	then
+		echo "-- building conda environment"
+		conda env create -f src/SOPRANO/local.yml
+    echo "-- activating soprano"
+    conda activate soprano-dev
+	else
+		echo "-- soprano environment detected"
+    echo "-- activating soprano"
+    conda activate soprano-dev
+		echo "-- checking for updates to environment"
+		conda env update --file $yml_path --prune
+	fi
+
+else
+	echo "No conda variant available to build environment!"
+fi
+
+pip install -e .[dev]
