2b6274ff08cc0efe8df1324e9b067403d56ee9e8
KareemMarzouk <bkmarzouk@outlook.com>
2023-08-23 12:56:07 +0100

    generalise method for detecting whether exec found & add secondary check for whether inside of current conda env

--- src/SOPRANO/sh_utils/setup_conda.py
+++ src/SOPRANO/sh_utils/setup_conda.py
@@ -143,13 +143,33 @@ def running_soprano_env():
         return False
 
 
+def _check_exec(exec_name):
+    """
+    Check executable is found and inside of conda environment
+    :param exec_name: name of executable, e.g. vep
+    :return: True if found and inside current conda env, else False
+    """
+
+    # Initial check on exit code for executable
+    which_exec = subprocess.run(["which", exec_name], stdout=subprocess.PIPE)
+    if which_exec.returncode != 0:
+        return False
+
+    # Build expected path
+    conda_env_path = os.environ["CONDA_PREFIX"]
+    expected_path = pathlib.Path(conda_env_path).joinpath("bin")
+    expected_path = expected_path.joinpath(exec_name)
+
+    # Decode detected path
+    detected_path = which_exec.stdout.decode()
+    detected_path = detected_path
+
+    return expected_path.as_posix().strip("\n") == detected_path.strip("\n")
+
+
 def vep_installed():
-    which_vep = subprocess.run(["which", "vep"], stdout=subprocess.PIPE)
-    # TODO: add secondary check that vep is inside of conda env
-    return which_vep.returncode == 0
+    return _check_exec("vep")
 
 
 def bedtools_installed():
-    which_vep = subprocess.run(["which", "bedtools"], stdout=subprocess.PIPE)
-    # TODO: add secondary check that bedtools is inside of conda env
-    return which_vep.returncode == 0
+    return _check_exec("bedtools")
