8cb930edb8e5a230707469b76ef952f92e704799
KareemMarzouk <bkmarzouk@outlook.com>
2023-08-23 10:48:33 +0100

    setup.sh update -> source environment constructor from src/

--- setup.sh
+++ setup.sh
@@ -1,58 +1,10 @@
 #!/bin/bash
 
-# Mamba/conda path
-_mamba_path=$(which mamba)
-_conda_path=$(which conda)
-_env_grep=$(conda env list | grep "soprano-dev")
+# Keep track of project root
+PROJECT_ROOT="$( cd -- "$(dirname -- "$0")" >/dev/null 2>&1 || exit ; pwd -P )"
 
-yml_path="src/SOPRANO/local.yml"
+# Installation helpers dir
+INSTALLERS_DIR_PATH="src/SOPRANO/bash_installers"
 
-function deactivate_all() {
-  for _ in $(seq "${CONDA_SHLVL}")
-  do
-    conda deactivate
-  done
-}
-
-if [ -n "$_mamba_path" ]
-then
-	echo "-- mamba detected"
-  deactivate_all
-
-	if [ -z "$_env_grep" ]
-	then
-		echo "-- building environment"
-		mamba env create -f $yml_path
-    echo "-- activating soprano"
-    mamba activate soprano-dev
-	else
-		echo "-- soprano environment detected"
-    echo "-- activating soprano"
-    mamba activate soprano-dev
-		echo "-- checking for updates to environment"
-		mamba env update --file $yml_path --prune
-	fi
-
-elif [ -n "$_conda_path" ]
-then
-	echo "-- conda detected"
-  deactivate_all
-	if [ -z "$_env_grep" ]
-	then
-		echo "-- building conda environment"
-		conda env create -f src/SOPRANO/local.yml
-    echo "-- activating soprano"
-    conda activate soprano-dev
-	else
-		echo "-- soprano environment detected"
-    echo "-- activating soprano"
-    conda activate soprano-dev
-		echo "-- checking for updates to environment"
-		conda env update --file $yml_path --prune
-	fi
-
-else
-	echo "No conda variant available to build environment!"
-fi
-
-pip install -e .[dev]
+# Configure conda environment and install repository
+source "$INSTALLERS_DIR_PATH/.setup_env.sh"
