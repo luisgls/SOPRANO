4d0584891e247af0907974b24cbc16270dc4a042
KareemMarzouk <bkmarzouk@outlook.com>
2023-08-25 09:52:11 +0100

    remove redundent condarc efforts

--- src/SOPRANO/env_utils/config_conda_env.py
+++ src/SOPRANO/env_utils/config_conda_env.py
@@ -2,65 +2,6 @@ import os
 import pathlib
 import subprocess
 
-SH_UTILS_DIR = pathlib.Path(__file__).resolve().parent
-SOPRANO_SRC_DIR = SH_UTILS_DIR.parent
-
-# This should be pointed to within the ~/.condarc file
-# TODO: no longer needed...
-CONDA_DIR = SOPRANO_SRC_DIR / "conda_env"
-
-
-def _create_new_condarc(conda_env_location: str, condarc_path: str) -> None:
-    # TODO: no longer needed...
-    condarc_lines = [
-        "envs_dirs:\n",
-        "  - ~/.conda/envs\n",
-        f"  - {conda_env_location}\n",
-    ]
-
-    with open(condarc_path, "w") as f:
-        f.writelines(condarc_lines)
-
-
-def _update_condarc(conda_env_location: str, condarc_path: str) -> None:
-    # TODO: no longer needed...
-    local_env_in_rc = False
-    envs_clause_in_rc = False
-    envs_clause = "envs_dirs:\n"
-    envs_clause_idx = -1
-
-    # Read lines from condarc file
-    with open(condarc_path, "r") as f:
-        condarc_lines = f.readlines()
-
-    # Iterate over lines (and count their index)
-    for line_idx, line in enumerate(condarc_lines):
-        if conda_env_location in line:
-            local_env_in_rc = True
-        if envs_clause in line:
-            envs_clause_in_rc = True
-            envs_clause_idx = line_idx
-
-    if not local_env_in_rc:
-        if envs_clause_in_rc:
-            # Get yml tab spacing from following line
-            # crate new line with custom location
-            # and insert into list of original lines
-            yml_tab_spacing = condarc_lines[envs_clause_idx + 1].split("-")[0]
-            new_line = f"{yml_tab_spacing}- {conda_env_location}\n"
-            condarc_lines.insert(envs_clause_idx + 2, new_line)
-        else:
-            # Insert envs clause into start of rc file
-            # then insert standard and custom locations line
-            std_line = "  - ~/.conda/envs\n"
-            new_line = f"  - {conda_env_location}\n"
-            condarc_lines.insert(0, envs_clause)
-            condarc_lines.insert(1, std_line)
-            condarc_lines.insert(2, new_line)
-
-        with open(condarc_path, "w") as f:
-            f.writelines(condarc_lines)
-
 
 def _has_conda():
     which_conda = subprocess.run(["which", "conda"], stdout=subprocess.PIPE)
@@ -70,69 +11,6 @@ def _has_conda():
 def _has_mamba():
     which_mamba = subprocess.run(["which", "mamba"], stdout=subprocess.PIPE)
     return which_mamba.returncode == 0
-
-
-def _conda_env_exists(_flavor="conda") -> bool:
-    # TODO: no longer needed...
-    grep_env = subprocess.run([_flavor, "env", "list"], stdout=subprocess.PIPE)
-    return grep_env.returncode == 0
-
-
-def _update_conda_env(conda_env_location: str):
-    # TODO: no longer needed...
-    pass
-
-
-class CondaEnvironmentFailure(Exception):
-    pass
-
-
-def _build_conda_env(conda_env_location: str):
-    # TODO: no longer needed...
-    yml_path = SOPRANO_SRC_DIR / "local.yml"
-
-    if _conda_env_exists():
-        #     _update_conda_env(conda_env_location)
-        # else:
-        conda_exec = "mamba" if _has_mamba() else "conda"
-
-        soprano_env_location = os.path.join(conda_env_location, "SOPRANO")
-
-        if os.path.exists(soprano_env_location):
-            raise OSError(
-                f"'SOPRANO' environment directory already exists: "
-                f"{soprano_env_location}"
-            )
-
-        print("-- Building conda environment, this will take some time...")
-        os.environ["CONDA_ALWAYS_YES"] = "true"
-        env_build_output = subprocess.run(
-            [
-                f"{conda_exec}",
-                "env",
-                "create",
-                "--prefix",
-                f"{soprano_env_location}",
-                "--file",
-                f"{yml_path}",
-            ],
-            stdout=subprocess.PIPE,
-        )
-
-        if env_build_output.returncode != 0:
-            raise CondaEnvironmentFailure(
-                "Non-zero exit code generated from conda environment build."
-            )
-
-
-def prepare_condarc():
-    # TODO: no longer needed...
-    conda_rc_path = pathlib.Path.home() / ".condarc"
-
-    if not conda_rc_path.exists():
-        _create_new_condarc(CONDA_DIR.as_posix(), conda_rc_path.as_posix())
-    else:
-        _update_condarc(CONDA_DIR.as_posix(), conda_rc_path.as_posix())
 
 
 def running_soprano_env():
--- tests/test_configuration/test_setup_conda.py
+++ tests/test_configuration/test_setup_conda.py
@@ -1,87 +1,11 @@
-import pathlib
-import tempfile
-
 import pytest
 
 from SOPRANO.env_utils import config_conda_env
 
 
-@pytest.mark.dependency(name="create_rc")
-def test__create_new_condarc():
-    with tempfile.TemporaryDirectory() as tmp_dir:
-        condarc_path = pathlib.Path(tmp_dir).joinpath(".condarc").as_posix()
-        mock_env_location = pathlib.Path.cwd().as_posix()
-
-        expected_lines = [
-            "envs_dirs:\n",
-            "  - ~/.conda/envs\n",
-            f"  - {mock_env_location}\n",
-        ]
-
-        config_conda_env._create_new_condarc(mock_env_location, condarc_path)
-        with open(condarc_path, "r") as f:
-            written_lines = f.readlines()
-
-        for expected, written in zip(expected_lines, written_lines):
-            assert expected == written, (expected, written)
-
-
-@pytest.mark.dependency(name="update_rc")
-def test__update_condarc():
-    mock_env_location = pathlib.Path.cwd().as_posix()
-
-    with tempfile.TemporaryDirectory() as tmp_dir:
-        # Test 1: Existing envs block but no soprano env loc
-
-        condarc_path = pathlib.Path(tmp_dir).joinpath(".condarc").as_posix()
-
-        indentation = "  "
-
-        partial_lines = ["envs_dirs:\n", f"{indentation}- ~/.conda/envs\n"]
-
-        with open(condarc_path, "w") as f:
-            f.writelines(partial_lines)
-
-        expected_lines = partial_lines + [
-            f"{indentation}- {mock_env_location}\n"
-        ]
-
-        config_conda_env._update_condarc(mock_env_location, condarc_path)
-
-    with tempfile.TemporaryDirectory() as tmp_dir:
-        # Test 2: Existing rc but no envs block
-
-        condarc_path = pathlib.Path(tmp_dir).joinpath(".condarc").as_posix()
-        pkgs_lines = ["pkgs_dirs:\n", "  - /opt/anaconda/pkgs\n"]
-
-        with open(condarc_path, "w") as f:
-            f.writelines(pkgs_lines)
-
-        config_conda_env._update_condarc(mock_env_location, condarc_path)
-
-        # pkgs lines should be prepended by envs lines
-        expected_lines = expected_lines + pkgs_lines
-
-        with open(condarc_path, "r") as f:
-            written_lines = f.readlines()
-
-        for expected, written in zip(expected_lines, written_lines):
-            assert expected == written, (expected, written)
-
-
-@pytest.mark.dependency(depends=["create_rc", "update_rc"])
-def test_prepare_condarc():
-    pass  # assert False
-
-
 @pytest.mark.dependency(name="has_conda")
 def test__has_conda():
     assert config_conda_env._has_conda() or config_conda_env._has_mamba()
-
-
-@pytest.mark.dependency(depends=["has_conda"])
-def test__build_conda_env():
-    pass  # assert False
 
 
 def test_running_soprano_env():
