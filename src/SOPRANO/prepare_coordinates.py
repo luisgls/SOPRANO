7a22ad48df4aeb6247256b35431204516edd949f
KareemMarzouk <bkmarzouk@outlook.com>
2023-08-25 18:44:07 +0100

    more details in docstring for _filter_transcript_file

--- src/SOPRANO/prepare_coordinates.py
+++ src/SOPRANO/prepare_coordinates.py
@@ -4,10 +4,10 @@ import tempfile
 
 
 def _filter_transcript_file(
-    bed_file: pathlib.PosixPath,
-    transcript_file: pathlib.PosixPath,
-    cache_dir: pathlib.PosixPath,
-) -> pathlib.PosixPath:
+    bed_file: pathlib.Path,
+    transcript_file: pathlib.Path,
+    cache_dir: pathlib.Path,
+) -> pathlib.Path:
     """
 
     Implementation of methods in line 92-93
@@ -15,6 +15,29 @@ def _filter_transcript_file(
     cut -f1 $BED | sort -u |
         fgrep -w -f - $SUPA/ensemble_transcript_protein.length >
             $TMP/$NAME.protein_length_filt.txt
+
+    Bedfile format looks like:
+    ENST00000000233 9       18
+    ENST00000000233 37      46
+    ENST00000000233 98      111
+    ENST00000000233 115     124
+    ENST00000000233 164     177
+    ...
+
+    cut -f1 $BED extracts first col, e.g.
+    ENST00000000233
+    ENST00000000233
+    ENST00000000233
+    ENST00000000233
+    ENST00000000233
+
+    sort -u extracts the unique lines
+
+    fgrep -w -f <file>, -w matches only whole words, -f take patterns from file
+
+    Hence, the operation strips the first column ENST<number>, looks for all
+    the unique identifiers, then regex matches the identifiers against those
+    in the transcript files.
 
     :param bed_file: Bed file representation of target protein regions
     :param transcript_file: Transcript file to filter
@@ -39,7 +62,16 @@ def _filter_transcript_file(
         f"{transcript_filt_file}"
     )
     cli_cmd = cli_str_cmd.split(sep=" ")
-    subprocess.run(cli_cmd)
+
+    print(cli_cmd)
+
+    x = subprocess.run(cli_cmd, stdout=subprocess.PIPE)
+    print(x.stdout)
+    print(x.stderr)
+
+    # TODO: Clearly need generalization of piping since
+    #       this doesn't work out of the box with subprocess
+    #       without invoking shell :S
 
     return transcript_filt_file
 
