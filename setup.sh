f3813c14030f5a39f6f1a35b01b66aab5455c3d1
KareemMarzouk <bkmarzouk@outlook.com>
2023-08-25 10:22:59 +0100

    add cmd line option for installation (dev or ci). Add deps for ci in toml.

--- setup.sh
+++ setup.sh
@@ -6,5 +6,19 @@ PROJECT_ROOT="$( cd -- "$(dirname -- "$0")" >/dev/null 2>&1 || exit ; pwd -P )"
 # Installation helpers dir
 INSTALLERS_DIR_PATH="src/SOPRANO/bash_installers"
 
+if [ "$#" -eq 0 ]
+then
+  echo "-- installing dependencies for dev"
+  DEPS="dev"
+elif [ "$1" == "dev" ] || [ "$1" == "ci" ]
+then
+  echo "-- installing dependencies for $1"
+  DEPS=$1
+else
+  echo "-- unrecognized dependencies argument. (defaulting to dev)"
+  DEPS="dev"
+fi
+_PIP_CMD="pip install -e .[$DEPS]"
+
 # Configure conda environment and install repository
 source "$INSTALLERS_DIR_PATH/.setup_env.sh"
