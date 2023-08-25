b3422e15fa030ef68fd8c84d79bc83ddf7ba3db9
KareemMarzouk <bkmarzouk@outlook.com>
2023-08-25 09:54:58 +0100

    mark dependencies for conda installations

--- tests/test_configuration/test_setup_conda.py
+++ tests/test_configuration/test_setup_conda.py
@@ -8,13 +8,16 @@ def test__has_conda():
     assert config_conda_env._has_conda() or config_conda_env._has_mamba()
 
 
+@pytest.mark.dependency(depends=["has_conda"])
 def test_running_soprano_env():
     assert config_conda_env.running_soprano_env()
 
 
+@pytest.mark.dependency(depends=["has_conda"])
 def test_vep_installed():
     assert config_conda_env.vep_installed()
 
 
+@pytest.mark.dependency(depends=["has_conda"])
 def test_bedtools_installed():
     assert config_conda_env.bedtools_installed()
