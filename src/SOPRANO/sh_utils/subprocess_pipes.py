d66b54aef3a944b76024f4f2da9ba570178c980a
KareemMarzouk <bkmarzouk@outlook.com>
2023-08-26 09:59:58 +0100

    dev and test and pipe wrapper for subprocesses

--- src/SOPRANO/sh_utils/subprocess_pipes.py
+++ src/SOPRANO/sh_utils/subprocess_pipes.py
@@ -8,7 +8,13 @@ def process_output_to_string(ps: subprocess.CompletedProcess) -> str:
 
 def process_output_to_file(
     ps: subprocess.CompletedProcess, path: pathlib.Path, overwrite=False
-):
+) -> None:
+    """
+
+    :param ps: complete subprocess
+    :param path: Path to write stdout to
+    :param overwrite: if False, exception raised if output_path exists
+    """
     if path.exists() and not overwrite:
         raise FileExistsError(path)
 
@@ -17,48 +23,28 @@ def process_output_to_file(
 
 
 def pipe(
-    args_1: list | tuple,
-    args_2: list | tuple,
-    return_output=False,
-    output_path=None,
-    overwrite=False,
-):
-    ps_1 = subprocess.run(args_1, capture_output=True)
-    ps_2 = subprocess.run(args_2, input=ps_1.stdout, capture_output=True)
-
-    if output_path is not None:
-        process_output_to_file(ps_2, path=output_path, overwrite=overwrite)
-
-    if return_output:
-        return process_output_to_string(ps_2)
-
-
-def rec_pipe(
-    *args: list | tuple,
-    _input=None,
-    return_output=False,
-    output_path=None,
-    overwrite=False,
-):
+    *args: list | tuple, _input=None, output_path=None, overwrite=False
+) -> str:
     """
     Recursive pipe of cmdline args through subprocesses
     :param args: comma seperated lists or tuples representing cmds
     :param _input: input to pipe into leading command from args
-    :return:
+    :param output_path: Path to write stdout to
+    :param overwrite: if False, exception raised if output_path exists
+    :return: string representation of stdout from cumulative piped processes
     """
 
     ps = subprocess.run(args[0], input=_input, capture_output=True)
 
-    print(args[0], ps.stdout)
-
     if len(args) > 1:
-        return rec_pipe(*args[1:], _input=ps.stdout)
+        return pipe(
+            *args[1:],
+            _input=ps.stdout,
+            output_path=output_path,
+            overwrite=overwrite,
+        )
     else:
         if output_path is not None:
             process_output_to_file(ps, path=output_path, overwrite=overwrite)
 
-        if return_output:
-            return process_output_to_string(ps)
-
-
-# rec_pipe(["ls", "-l"], ["grep", "xr"], ["grep", "src"])
+        return process_output_to_string(ps)
--- tests/test_units/test_subprocess_pipes.py
+++ tests/test_units/test_subprocess_pipes.py
@@ -38,3 +38,34 @@ def test_process_output_to_file():
         # Check expectation
         assert len(lines) == 1
         assert lines[0] == "123"
+
+
+def test_pipe():
+    with tempfile.TemporaryDirectory() as _tmpdir:
+        tmpdir = pathlib.Path(_tmpdir)
+
+        # write files with suffix 00, 05, 10, 15 (for grep tests)
+        for idx in range(4):
+            file_name = tmpdir.joinpath("file_%02d" % (idx * 5))
+            with open(file_name, "w"):
+                pass
+            assert pathlib.Path(file_name).exists()
+
+        assert sbp.pipe(["ls", "-1", tmpdir], ["grep", "05"]) == "file_05"
+
+        output_path = tmpdir.joinpath("out")
+
+        sbp.pipe(
+            ["ls", "-1", tmpdir],
+            ["grep", "file"],
+            ["grep", "0"],
+            output_path=output_path,
+        )
+
+        expected_lines = ["file_00", "file_05"]
+
+        with open(output_path, "r") as f:
+            written_lines = f.readlines()
+
+        for e, w in zip(expected_lines, written_lines):
+            assert e == w.strip()
