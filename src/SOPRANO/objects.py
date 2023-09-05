4b7cda6ddf2904e41e4269047a35ab45095996b2
KareemMarzouk <bkmarzouk@outlook.com>
2023-09-05 11:51:32 +0100

    minor refactoring of existing methods. more complete treatment of input parameters. classmethod to intialize parameters from namespace

--- src/SOPRANO/objects.py
+++ src/SOPRANO/objects.py
@@ -1,4 +1,5 @@
 import pathlib
+from argparse import Namespace
 from dataclasses import dataclass
 
 
@@ -12,18 +13,19 @@ class TranscriptPaths:
     protein_transcript_length: pathlib.Path
 
 
-@dataclass(frozen=True)
-class _GenomicPaths:
-    sizes: pathlib.Path
-    fasta: pathlib.Path
-
-
 EnsemblTranscripts = TranscriptPaths(
     transcript_length=_data_dir().joinpath("ensemble_transcript.length"),
     protein_transcript_length=_data_dir().joinpath(
         "ensemble_transcript_protein.length"
     ),
 )
+
+
+@dataclass(frozen=True)
+class _GenomicPaths:
+    sizes: pathlib.Path
+    fasta: pathlib.Path
+
 
 GRCh37 = _GenomicPaths(
     sizes=_data_dir().joinpath("chrom_GRCh37.sizes"),
@@ -60,13 +62,13 @@ class AnalysisPaths:
         self,
         analysis_name: str,
         bed_path: pathlib.Path,
-        tmpdir: pathlib.Path,
-        target_regions_path: pathlib.Path | None = None,
+        cache_dir: pathlib.Path,
+        target_regions: pathlib.Path | None = None,
     ):
-        self.name = analysis_name
+        self.analysis_name = analysis_name
         self.bed_path = bed_path
-        self.target_regions_path = target_regions_path
-        self.tmpdir = tmpdir
+        self.target_regions_path = target_regions
+        self.cache_dir = cache_dir
 
         # Transcripts
         self.filtered_protein_transcript = self._cached_path(
@@ -96,18 +98,62 @@ class AnalysisPaths:
         )
 
     def _cached_path(self, *extensions):
-        return cache_path_builder(self.tmpdir, self.name, *extensions)
+        return cache_path_builder(
+            self.cache_dir, self.analysis_name, *extensions
+        )
+
+
+_NAMESPACE_KEYS = (
+    "analysis_name",
+    "input_path",
+    "bed_path",
+    "cache_dir",
+    "target_regions",
+    "transcript",
+    "protein_transcript",
+    "use_ssb192",
+    "use_random",
+    "exclude_drivers",
+)
 
 
 class Parameters(AnalysisPaths):
     def __init__(
         self,
         analysis_name: str,
+        input_path: pathlib.Path,
         bed_path: pathlib.Path,
-        tmpdir: pathlib.Path,
-        target_regions_path: pathlib.Path | None = None,
-        transcripts: TranscriptPaths = EnsemblTranscripts,
+        cache_dir: pathlib.Path,
+        target_regions: pathlib.Path,
+        use_ssb192: bool,
+        use_random: bool,
+        exclude_drivers: bool,
+        transcripts: TranscriptPaths,
     ):
-        super().__init__(analysis_name, bed_path, tmpdir, target_regions_path)
+        super().__init__(analysis_name, bed_path, cache_dir, target_regions)
 
         self.transcripts = transcripts
+        self.use_ssb192 = use_ssb192
+        self.use_random = use_random
+        self.exclude_drivers = exclude_drivers
+
+    @classmethod
+    def from_namespace(cls, namespace: Namespace):
+        for k in namespace.__dict__.keys():
+            assert k in _NAMESPACE_KEYS, k
+
+        transcripts = TranscriptPaths(
+            namespace.transcript, namespace.protein_transcript
+        )
+
+        return cls(
+            namespace.analysis_name,
+            namespace.input_path,
+            namespace.bed_path,
+            namespace.cache_dir,
+            namespace.target_regions,
+            namespace.use_ssb192,
+            namespace.use_random,
+            namespace.exclude_drivers,
+            transcripts,
+        )
