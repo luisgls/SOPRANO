2ce8baef09619eece88473655754f4f25843ea92
KareemMarzouk <bkmarzouk@outlook.com>
2023-09-18 10:29:51 +0100

    move path defs for tcga ssb192 test case into fixture

--- tests/test_units/conftest.py
+++ tests/test_units/conftest.py
@@ -1,2 +1,3 @@
 from _step2_fixtures import step_2_defs  # noqa: F401
 from _step3_fixtures import step_3_defs  # noqa: F401
+from _step6_fixtures import tcga_05_4396_ssb192_cfg  # noqa: F401
--- tests/test_units/test_dnds.py
+++ tests/test_units/test_dnds.py
@@ -1,44 +1,24 @@
-import pathlib
-
 import pandas as pd
 
 import SOPRANO.dnds
-from SOPRANO.objects import AnalysisPaths
-
-# TODO: Fixture....
-data_dir = pathlib.Path(__file__).parent.joinpath("dnds_data")
-data_epitopes_path = data_dir.joinpath("TCGA-05-4396.data.epitopes")
-epi_nans_path = data_dir.joinpath("TCGA-05-4396.epitopes.nans")
-intra_nans_path = data_dir.joinpath("TCGA-05-4396.intra_epitopes.nans")
-intron_path = data_dir.joinpath("TCGA-05-4396.intron.rate")
-
-merged_path = data_dir.joinpath("merged.tsv")
-sites_extra_path = data_dir.joinpath("sites_extra.tsv")
-sites_intra_path = data_dir.joinpath("sites_intra.tsv")
-
-paths = AnalysisPaths(
-    "tcga_test", data_epitopes_path, data_epitopes_path, pathlib.Path(".")
-)  # Just hacking something together here...
-
-paths.data_epitopes = data_epitopes_path
-paths.epitope_nans = epi_nans_path
-paths.intra_epitope_nans = intra_nans_path
-paths.intron_rate = intron_path
 
 
-def test__preprocess_dfs():
+def test__preprocess_dfs(tcga_05_4396_ssb192_cfg):
+    (
+        paths,
+        merged_path,
+        sites_extra_path,
+        sites_intra_path,
+    ) = tcga_05_4396_ssb192_cfg
+
     merged, sites_extra, sites_intra = SOPRANO.dnds._preprocess_dfs(paths)
 
-    test_merged_path = data_dir.joinpath("merged.tsv")
-    test_extra_path = data_dir.joinpath("sites_extra.tsv")
-    test_intra_path = data_dir.joinpath("sites_intra.tsv")
-
-    assert merged.head().equals(pd.read_csv(test_merged_path, delimiter="\t"))
+    assert merged.head().equals(pd.read_csv(merged_path, delimiter="\t"))
     assert sites_extra.head().equals(
-        pd.read_csv(test_extra_path, delimiter="\t")
+        pd.read_csv(sites_extra_path, delimiter="\t")
     )
     assert sites_intra.head().equals(
-        pd.read_csv(test_intra_path, delimiter="\t")
+        pd.read_csv(sites_intra_path, delimiter="\t")
     )
 
 
@@ -150,27 +130,3 @@ def test__compute_kaks_intron():
 def test__compute_conf_interval():
     # TODO: Implement
     pass
-
-
-merged, extra, intra = SOPRANO.dnds._preprocess_dfs(paths)
-
-muts = SOPRANO.dnds._compute_mutation_counts(merged)
-
-vars = SOPRANO.dnds._define_variables(muts, extra, intra)
-#
-# cl_epi = dnds_intron._compute_conf_interval(vars, "extra", "katz")
-# cl_nonepi = dnds_intron._compute_conf_interval(vars, "intra", "katz")
-# cl_intron = dnds_intron._compute_conf_interval(vars, "intron", "katz")
-#
-# pval_nonepi = dnds_intron._compute_pvalue(vars, cl_nonepi, "intra")
-# pval_intron = dnds_intron._compute_pvalue(vars, cl_nonepi, "intron")
-
-# print("epitope", cl_epi)
-# print("non-epitope", cl_nonepi)
-# print("intron", cl_intron)
-# print("pval non-epitope", pval_nonepi)
-# print("pval intron", pval_intron)
-
-res = SOPRANO.dnds._compute_coverage(paths)
-#
-# print("x" in muts)
