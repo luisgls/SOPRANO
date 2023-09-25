de4cc88075a0a62df02086a30c92b6bdf629b930
KareemMarzouk <bkmarzouk@outlook.com>
2023-09-25 16:05:28 +0100

    input and bed options in app

--- src/SOPRANO/app.py
+++ src/SOPRANO/app.py
@@ -3,9 +3,9 @@ import pathlib
 import streamlit as st
 
 # Find genome options
-homo_sapiens_dir = pathlib.Path(__file__).parent / "data" / "homo_sapiens"
+_HOMO_SAPIENS_DIR = pathlib.Path(__file__).parent / "data" / "homo_sapiens"
 
-_GENOME_DIRS = [item for item in homo_sapiens_dir.glob("*") if item.is_dir()]
+_GENOME_DIRS = [item for item in _HOMO_SAPIENS_DIR.glob("*") if item.is_dir()]
 
 _GENOME_NAMES = [
     "{} - Ensembl release {}".format(*x.name.split("_")[::-1])
@@ -15,6 +15,12 @@ _GENOME_NAMES = [
 _GENOME_DICT = {
     name: dir_path for name, dir_path in zip(_GENOME_NAMES, _GENOME_DIRS)
 }
+
+_ANNO_DIR = pathlib.Path(__file__).parent / "examples"
+_ANNO_OPTIONS = {x.name: x for x in _ANNO_DIR.glob("*.anno*")}
+
+_BED_DIR = pathlib.Path(__file__).parent / "immunopeptidomes" / "human"
+_BED_OPTIONS = {x.name: x for x in _BED_DIR.glob("*.bed")}
 
 # Init app
 st.title("SOPRANO")
@@ -28,3 +34,15 @@ toplevel_path = (
     _GENOME_DICT[genome_selection] / f"Homo_sapiens.{ref_id}.dna.toplevel.fa"
 )
 st.text(f"Selected: {toplevel_path}")
+
+# VEP annotated file
+input_selection = st.selectbox(
+    "Select a VEP annotated file:", _ANNO_OPTIONS.keys()
+)
+input_path = _ANNO_OPTIONS[input_selection]
+st.text(f"Selected: {input_path}")
+
+# BED protein transcript file
+bed_selection = st.selectbox("Select a BED protein file:", _BED_OPTIONS.keys())
+bed_path = _BED_OPTIONS[bed_selection]
+st.text(f"Selected: {bed_path}")
