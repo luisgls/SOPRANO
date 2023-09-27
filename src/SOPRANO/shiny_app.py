1ba5c8e71729b090a69d60adc1aff8c8966608af
KareemMarzouk <bkmarzouk@outlook.com>
2023-09-27 10:54:20 +0100

    shiny (comparitive to streamlit implementation)

--- src/SOPRANO/shiny_app.py
+++ src/SOPRANO/shiny_app.py
@@ -1,14 +1,12 @@
-# import argparse
+import argparse
 import os
 import pathlib
+import time
 
-# import pandas as pd
-from shiny import App, render, ui
+from shiny import App, reactive, render, ui
 
-# import time
+from SOPRANO import objects, run_local_ssb_selection
 
-
-# from SOPRANO import objects, run_local_ssb_selection
 _DEFAULT_CACHE = pathlib.Path(__file__).parent.parent.parent / "pipeline_cache"
 
 # Cache location
@@ -38,13 +36,13 @@ _BED_OPTIONS = {x.name: x for x in _BED_DIR.glob("*.bed")}
 _SUB_OPTIONS = ("SSB7", "SSB192")
 
 
-def process_genome_selection(genome_selection: str) -> pathlib.Path:
+def process_genome_selection(genome_selection: str) -> tuple:
     ref_id, rel_id = genome_selection.split(" - Ensembl release ")
     toplevel_path = (
         _GENOME_DICT[genome_selection]
         / f"Homo_sapiens.{ref_id}.dna.toplevel.fa"
     )
-    return toplevel_path
+    return toplevel_path, ref_id, rel_id
 
 
 def process_input_selection(input_selection: str) -> pathlib.Path:
@@ -56,7 +54,7 @@ def process_bed_selection(bed_selection: str) -> pathlib.Path:
 
 
 def process_subs_selection(subs_selection: str) -> bool:
-    return int(subs_selection) == 192
+    return subs_selection == "SSB192"
 
 
 def process_job_name(job_name: str) -> pathlib.Path:
@@ -98,6 +96,8 @@ app_ui = ui.page_fluid(
     ui.input_numeric("random_seed", "Random Seed", value=-1, min=-1),
     ui.input_text("job_name", "Analysis Name"),
     ui.output_text_verbatim("c_location"),
+    ui.input_action_button("run_pipeline", "Run Pipeline"),
+    ui.output_text_verbatim("result", placeholder=True),
 )
 
 
@@ -105,7 +105,7 @@ def server(input, output, session):
     @output
     @render.text
     def g_selection():
-        return process_genome_selection(input.genome_selection())
+        return process_genome_selection(input.genome_selection())[1]
 
     @output
     @render.text
@@ -121,6 +121,36 @@ def server(input, output, session):
     @render.text
     def c_location():
         return process_job_name(input.job_name())
+
+    @output
+    @render.text
+    @reactive.event(input.run_pipeline)
+    def result():
+        namespace = argparse.Namespace(
+            analysis_name=input.job_name(),
+            input_path=process_input_selection(input.input_selection()),
+            bed_path=process_bed_selection(input.bed_selection()),
+            cache_dir=process_job_name(input.job_name()),
+            target_regions=None,  # TODO: Fix
+            use_ssb192=process_subs_selection(input.subs_selection()),
+            use_random=input.use_random(),
+            exclude_drivers=input.exclude_drivers(),
+            seed=input.random_seed(),
+            transcript=objects.EnsemblTranscripts.transcript_length,
+            protein_transcript=objects.EnsemblTranscripts.protein_transcript_length,
+            transcript_ids=objects.EnsemblTranscripts.transcript_fasta,
+            genome_ref=process_genome_selection(input.genome_selection())[1],
+            release=process_genome_selection(input.genome_selection())[2],
+        )
+
+        params = objects.Parameters.from_namespace(namespace)
+        params.cache_dir.mkdir(exist_ok=True)
+
+        t_start = time.time()
+        run_local_ssb_selection.main(namespace)
+        t_end = time.time()
+
+        return f"Pipeline run in {int(t_end - t_start)} seconds"
 
 
 app = App(app_ui, server)
