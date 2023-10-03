161d11ac683364a93e0c286f082690ae6b3ccb6a
KareemMarzouk <bkmarzouk@outlook.com>
2023-10-03 12:20:50 +0100

    generalize test for checking options

--- tests/test_units/test_app_utils.py
+++ tests/test_units/test_app_utils.py
@@ -2,19 +2,25 @@ from SOPRANO.utils.app_utils import get_annotated_input_options
 from SOPRANO.utils.path_utils import Directories
 
 
-def test_get_annotated_input_options():
-    tmp_name = "pytest.anno"
-    bed_name = "pytest.bed"
+def _check_detected(dir_method, options_method, ext):
+    tmp_name = f"pytest.{ext}"
+    other_name = "pytest.other"
 
-    tmp_path = Directories.app_annotated_inputs(tmp_name)
-    bed_path = Directories.app_annotated_inputs(bed_name)
+    tmp_path = dir_method(tmp_name)
+    other_path = dir_method(other_name)
     tmp_path.touch(exist_ok=False)
-    bed_path.touch(exist_ok=False)
+    other_path.touch(exist_ok=False)
 
-    options = get_annotated_input_options()
+    options = options_method()
 
     assert tmp_name in options.keys()
-    assert bed_name not in options.keys()
+    assert other_name not in options.keys()
 
     tmp_path.unlink(missing_ok=False)
-    bed_path.unlink(missing_ok=False)
+    other_path.unlink(missing_ok=False)
+
+
+def test_get_annotated_input_options():
+    _check_detected(
+        Directories.app_annotated_inputs, get_annotated_input_options, "anno"
+    )
