de86f7fafd1e633ec4a9a4d75703e7d539cad280
KareemMarzouk <bkmarzouk@outlook.com>
2023-10-12 17:22:54 +0100

    [skip ci] update tests with correct defs & clean up tmp files

--- tests/test_units/test_app_utils.py
+++ tests/test_units/test_app_utils.py
@@ -11,20 +11,20 @@ def _check_detected(dir_method, options_method, ext):
     tmp_path.touch(exist_ok=False)
     other_path.touch(exist_ok=False)
 
-    options = options_method()
-
-    assert tmp_name in options.keys()
-    assert options[tmp_name] == tmp_path
-    assert other_name not in options.keys()
-
-    tmp_path.unlink(missing_ok=False)
-    other_path.unlink(missing_ok=False)
+    try:
+        options = options_method()
+        assert tmp_name in options.keys()
+        assert options[tmp_name] == tmp_path
+        assert other_name not in options.keys()
+    finally:
+        tmp_path.unlink(missing_ok=False)
+        other_path.unlink(missing_ok=False)
 
 
 def test_get_annotated_input_options():
     _check_detected(
         Directories.app_annotated_inputs,
-        PipelineUIOptions.get_annotated_input_options,
+        PipelineUIOptions.annotated_mutations,
         "anno",
     )
 
@@ -32,7 +32,7 @@ def test_get_annotated_input_options():
 def test_get_immunopeptidome_options():
     _check_detected(
         Directories.app_immunopeptidomes,
-        PipelineUIOptions.get_immunopeptidome_options,
+        PipelineUIOptions.immunopeptidome,
         "bed",
     )
 
@@ -40,6 +40,6 @@ def test_get_immunopeptidome_options():
 def test_get_coordinate_options():
     _check_detected(
         Directories.app_coordinate_files,
-        PipelineUIOptions.get_coordinate_options,
+        PipelineUIOptions.coordinates,
         "bed",
     )
