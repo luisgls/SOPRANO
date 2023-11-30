9b1be88a1a3e88d05051e803414d1ba29bdae0bd
KareemMarzouk <bkmarzouk@outlook.com>
2023-11-30 11:24:21 +0000

    process single vcf file input

--- src/SOPRANO/utils/anno_utils.py
+++ src/SOPRANO/utils/anno_utils.py
@@ -4,27 +4,43 @@ from SOPRANO.utils.path_utils import Directories
 from SOPRANO.utils.sh_utils import pipe
 
 
-def find_vcf_files(vcf_sources_dir: Path):
+def find_vcf_files(vcf_source: Path):
     vcf_exts = {".vcf", ".VCF"}
     gz_exts = {".gz", ".GZ", ".Gz"}
 
     exts = {x + y for x in vcf_exts for y in gz_exts}
 
+    if vcf_source.is_file():
+        if vcf_source.suffix not in gz_exts:
+            raise ValueError(
+                f"Input source should be gzip compressed: " f"{vcf_source}"
+            )
+
+        if (vcf_source.with_suffix("")).suffix not in vcf_exts:
+            raise ValueError(
+                f"Input sourse should contain vcf extension: " f"{vcf_source}"
+            )
+
+        return [vcf_source]
+    elif not vcf_source.is_dir():
+        raise ValueError(
+            "Sources input must be a file of vcf.gz format, or a directory"
+            f"containing vcf.gz files. {vcf_source} is invalid."
+        )
+
     detected = []
 
     for ext in exts:
-        for vcf_file_path in vcf_sources_dir.glob(f"*{ext}"):
+        for vcf_file_path in vcf_source.glob(f"*{ext}"):
             detected.append(vcf_file_path)
 
     n_detected = len(detected)
 
     if n_detected > 0:
-        print(
-            f"Detected {n_detected} vcf files in {vcf_sources_dir.as_posix()}"
-        )
+        print(f"Detected {n_detected} vcf files in {vcf_source.as_posix()}")
     else:
         raise FileNotFoundError(
-            f"No VCF files detected in {vcf_sources_dir.as_posix()}"
+            f"No VCF files detected in {vcf_source.as_posix()}"
         )
 
     return detected
